330b52fddb5e70179603ea141f3182eb315897f9
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-11-12 13:19:01 +0300

    update tests

--- test.js
+++ test.js
@@ -4,48 +4,44 @@ var { URLSearchParams } = require('url')
 // expose modules to global
 global.URLSearchParams = URLSearchParams
 
-var fetch = sinon.fake()
+var fetch = sinon.fake.resolves()
 global.fetch = fetch
 
 // require module
 var rewrite = require('./rewrite')
 var fresource = require('./')
 
-var path = '/users/:id?sort=:orderBy'
-var Users = fresource(path)
+var Users = fresource('/users/:id?sort=:orderBy')
 
 tape('should rewrite path variables', t => {
-  var result = '/users/1'
-  t.equal(rewrite(path, { id: 1 }), result, 'rewrite')
+  t.equal(rewrite('/users/:id?sort=:orderBy', { id: 1 }), '/users/1', 'rewrite')
   t.end()
 })
 
 tape('should remove unused variables', t => {
-  var result = '/users'
-  t.equal(rewrite(path), result, 'rewrite w/o params')
-  t.equal(rewrite(path, {}), result, 'rewrite w/ params')
+  t.equal(rewrite('/users/:id?sort=:orderBy'), '/users', 'rewrite w/o params')
+  t.equal(rewrite('/users/:id?sort=:orderBy', {}), '/users', 'rewrite w/ params')
   t.end()
 })
 
 tape('should rewrite query params', t => {
-  var result = '/users?sort=name'
-  t.equal(rewrite(path, { orderBy: 'name' }), result, 'rewrite')
+  t.equal(rewrite('/users/:id?sort=:orderBy', { orderBy: 'name' }), '/users?sort=name', 'rewrite')
   t.end()
 })
 
 tape('should call fetch correctly', t => {
   Users.get()
-  t.true(fetch.calledWithMatch('/users'), 'get request')
+  t.true(fetch.calledWithExactly('/users'), 'get request')
 
   var params = { name: 'Ismail' }
   Users.save(params)
-  t.true(fetch.calledWithMatch('/users', { method: 'POST', body: JSON.stringify(params) }), 'post request')
+  t.true(fetch.calledWithExactly('/users', { method: 'POST', body: JSON.stringify(params) }), 'post request')
 
   params = { name: 'Ismail', id: 1 }
   Users.update(params)
-  t.true(fetch.calledWithMatch('/users/1', { method: 'PUT', body: JSON.stringify({ name: 'Ismail' }) }), 'put request')
+  t.true(fetch.calledWithExactly('/users/1', { method: 'PUT', body: JSON.stringify({ name: 'Ismail' }) }), 'put request')
 
   Users.delete({ id: 1 })
-  t.true(fetch.calledWithMatch('/users/1', { method: 'DELETE' }), 'delete request')
+  t.true(fetch.calledWithExactly('/users/1', { method: 'DELETE' }), 'delete request')
   t.end()
 })
